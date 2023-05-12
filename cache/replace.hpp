25fa3e71e234bf96e8a3da45000f98ae5e943481
Jinchi Han(韩金池） <59952508+HanJinChi@users.noreply.github.com>
2023-05-12 21:46:36 +0800

    add replacerFIFO and replacerLRU (#3)
    * add replacerFIFO and replacerLRU
    
    * Update cache.hpp
    
    Remove comment and the replacer's access hookup. The access behavior would be different with that on real hardware.
    
    ---------
    
    Co-authored-by: hanjinchi <hanjinchi00@outlook.com>
    Co-authored-by: Wei Song (宋威) <wsong83@gmail.com>

--- cache/replace.hpp
+++ cache/replace.hpp
@@ -1,6 +1,9 @@
 #ifndef CM_REPLACE_HPP_
 #define CM_REPLACE_HPP_
 
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
 #include "util/random.hpp"
 
 ///////////////////////////////////
@@ -8,13 +11,68 @@
 
 class ReplaceFuncBase
 {
+protected:
+  const uint32_t nset;
 public:
+  ReplaceFuncBase(uint32_t nset) : nset(nset) {};
   virtual uint32_t replace(uint32_t s, uint32_t *w) = 0;
-  virtual void read(uint32_t ai, uint32_t s, uint32_t w) = 0;
-  virtual void access(uint32_t ai, uint32_t s, uint32_t w) = 0;
-  virtual void write(uint32_t ai, uint32_t s, uint32_t w) = 0;
-  virtual void invalid(uint32_t ai, uint32_t s, uint32_t w) = 0;
+  virtual void access(uint32_t s, uint32_t w) = 0;
+  virtual void invalid(uint32_t s, uint32_t w) = 0;
   virtual ~ReplaceFuncBase() {}
+};
+
+template<int IW, int NW>
+class ReplaceFIFO : public ReplaceFuncBase
+{
+protected:
+  std::unordered_map<uint32_t, std::list<uint32_t> > used_map;
+  std::unordered_map<uint32_t, std::unordered_set<uint32_t> > free_map;
+
+public:
+  ReplaceFIFO() : ReplaceFuncBase(1ul<<IW) {}
+  virtual ~ReplaceFIFO() {}
+  virtual uint32_t replace(uint32_t s, uint32_t *w){
+    if(!free_map.count(s))
+      for(uint32_t i=0; i<IW; i++) free_map[s].insert(i);
+    if(free_map[s].size() > 0)
+      *w = *(free_map[s].begin());
+    else
+      *w = used_map[s].front();
+
+    return 0;
+  }
+  virtual void access(uint32_t s, uint32_t w) {
+    if(free_map[s].count(w)) {
+      free_map[s].erase(w);
+      used_map[s].push_back(w);
+    }
+  }
+  virtual void invalid(uint32_t s, uint32_t w){
+    used_map[s].remove(w);
+    free_map[s].insert(w);
+  }
+};
+
+template<int IW, int NW>
+class ReplaceLRU : public ReplaceFIFO<IW, NW>
+{
+public:
+  using ReplaceFIFO<IW,NW>::free_map;
+  using ReplaceFIFO<IW,NW>::used_map;
+  
+  ReplaceLRU() : ReplaceFIFO<IW,NW>() {}
+  ~ReplaceLRU() {}
+
+  virtual void access(uint32_t s, uint32_t w) {
+    if(free_map[s].count(w)) {
+      free_map[s].erase(w);
+      used_map[s].push_back(w);
+    }
+    else{
+      used_map[s].remove(w);
+      used_map[s].push_back(w);
+    }
+  }
 };
 
 #endif
