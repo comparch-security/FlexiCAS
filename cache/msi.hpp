0cdd1b9e367a0e09d2726e9bf18d0452b01190e0
Wei Song <wsong83@gmail.com>
2023-05-23 11:30:19 +0800

    bugfix: L1 no need to degrate modified to shared when serving a read.

--- cache/msi.hpp
+++ cache/msi.hpp
@@ -364,7 +364,7 @@ class CoreInterfaceMSI : public CoreInterfaceBase
       // fetch the missing block
       outer->acquire_req(addr, meta, data, cmd);
     }
-    Policy::meta_after_acquire(cmd, meta);
+
     if(cmd == Policy::cmd_for_core_write()) {
       meta->to_dirty();
       this->cache->replace_write(addr, ai, s, w, hit);
