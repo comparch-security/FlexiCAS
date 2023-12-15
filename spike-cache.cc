df9fd861813f9783bbf569a1c940cc8386fd567c
Wei Song <wsong83@gmail.com>
2023-12-15 12:51:29 +0800

    add an LLC dispatcher

--- spike-cache.cc
+++ spike-cache.cc
@@ -1,11 +1,10 @@
 #include "cache/memory.hpp"
 #include "util/cache_type.hpp"
+#include "cache/slicehash.hpp"
 
 #include <iostream>
 
 // intel coffe lake 9Gen: https://en.wikichip.org/wiki/intel/microarchitectures/coffee_lake
-
-#define NC 2
 
 // 32K 8W, both I and D
 #define L1IW 6
@@ -23,6 +22,7 @@
 
 static std::vector<CoreInterface *> core_data, core_inst;
 static SimpleTracer tracer(true);
+static int NC = 0;
 
 namespace flexicas {
 
@@ -48,29 +48,33 @@ namespace flexicas {
     }
   }
 
-  void init() {
+  void init(int ncore) {
+    NC = ncore;
     auto l1d = cache_gen_l1<L1IW, L1WN, void, MetadataBroadcastBase, ReplaceLRU, MSIPolicy, false, false, void, true>(NC, "l1d");
     core_data = get_l1_core_interface(l1d);
     auto l1i = cache_gen_l1<L1IW, L1WN, void, MetadataBroadcastBase, ReplaceLRU, MSIPolicy, false, true, void, true>(NC, "l1i");
     core_inst = get_l1_core_interface(l1i);
     auto l2 = cache_gen_l2_exc<L2IW, L2WN, void, MetadataBroadcastBase, ReplaceSRRIP, MSIPolicy, false, void, true>(NC, "l2");
-    auto l3 = cache_gen_llc_inc<L3IW, L3WN, void, MetadataDirectoryBase, ReplaceSRRIP, MESIPolicy, void, true>(1, "l3")[0];
+    auto l3 = cache_gen_llc_inc<L3IW, L3WN, void, MetadataDirectoryBase, ReplaceSRRIP, MESIPolicy, void, true>(NC, "l3");
+    auto dispatcher = new SliceDispatcher<SliceHashNorm<> >("disp", NC);
     auto mem = new SimpleMemoryModel<void,void,true>("mem");
 
     for(int i=0; i<NC; i++) {
       l1i[i]->outer->connect(l2[i]->inner, l2[i]->inner->connect(l1i[i]->outer, true));
       l1d[i]->outer->connect(l2[i]->inner, l2[i]->inner->connect(l1d[i]->outer));
-      l2[i]->outer->connect(l3->inner, l3->inner->connect(l2[i]->outer));
+      dispatcher->connect(l3[i]->inner);
+      l2[i]->outer->connect(dispatcher, l3[0]->inner->connect(l2[i]->outer));
+      if(i>0) for(int j=0; j<NC; j++) l3[i]->inner->connect(l2[j]->outer);
+      l3[i]->outer->connect(mem, mem->connect(l3[i]->outer));
 #ifdef EnTrace
       l1i[i]->attach_monitor(&tracer);
       l1d[i]->attach_monitor(&tracer);
       l2[i]->attach_monitor(&tracer);
+      l3[i]->attach_monitor(&tracer);
 #endif
     }
 
-    l3->outer->connect(mem, mem->connect(l3->outer));
 #ifdef EnTrace
-    l3->attach_monitor(&tracer);
     mem->attach_monitor(&tracer);
 #endif
 
