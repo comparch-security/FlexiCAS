59e056e490c088de6dc229990a885f2037259612
Wei Song <wsong83@gmail.com>
2023-12-15 11:39:27 +0800

    Add LLC dispatcher into regression
    * Let the intel regression case use an LLC dispatcher
    * Let dispatcher get the core count using a run-time parameter rather than a template parameter,
      as this is needed for better run-time configure of core count, especially needed by spike-flexicas.

--- cache/slicehash.hpp
+++ cache/slicehash.hpp
@@ -11,30 +11,34 @@
 
 class SliceHashBase
 {
+protected:
+  int slice;
 public:
+  SliceHashBase(int s) : slice(s) {}
   virtual uint32_t operator () (uint64_t addr) = 0;
 };
 
 /////////////////////////////////
 // normal (no hash)
-template<int NLLC, int BlkOfst>
+template<int BlkOfst = 6>
 class SliceHashNorm : public SliceHashBase
 {
 public:
-  virtual uint32_t operator () (uint64_t addr) { return (addr >> BlkOfst) % NLLC; }
+  SliceHashNorm(int s): SliceHashBase(s) {}
+  virtual uint32_t operator () (uint64_t addr) { return (addr >> BlkOfst) % slice; }
 };
 
 /////////////////////////////////
 // Inel complex address scheme (CAS)
-template<int NLLC> requires NLLC <= 8 && NLLC == ((~NLLC + 1) & NLLC) // NLLC <= 8 and is power of 2
 class SliceHashIntelCAS : public SliceHashBase
 {
   AddrXORHash hash;
 public:
-  SliceHashIntelCAS() {
-    if constexpr (NLLC == 2) hash.key({0x15f575440ull});
-    if constexpr (NLLC == 4) hash.key({0x6b5faa880ull, 0x35f575440ull});
-    if constexpr (NLLC == 8) hash.key({0x3cccc93100ull, 0x2eb5faa880ull, 0x1b5f575400ull});
+  SliceHashIntelCAS(int s) : SliceHashBase(s) {
+    if (s == 2)      hash.key({0x15f575440ull});
+    else if (s == 4) hash.key({0x6b5faa880ull, 0x35f575440ull});
+    else if (s == 8) hash.key({0x3cccc93100ull, 0x2eb5faa880ull, 0x1b5f575400ull});
+    else             assert(0 == "The number of slices must be equal to 2, 4 or 8!");
   }
 
   uint32_t virtual operator () (uint64_t addr) { return hash(addr); }
