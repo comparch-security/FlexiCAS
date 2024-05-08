8c9b534cb1f271bb78d6328c739b701155c56ded
wangzd12138 <wangzd12138@outlook.com>
2024-05-08 20:23:15 +0800

    Add randomized LLC and remap into regression

---
+++ regression/c2-l2-randomized.cpp
@@ -0,0 +1,38 @@
+#include "cache/memory.hpp"
+#include "util/cache_type.hpp"
+#include "util/regression.hpp"
+
+#define PAddrN 128
+#define SAddrN 64
+#define NCore 2
+#define TestN ((PAddrN + SAddrN) * NCore * 2)
+
+#define L1IW 4
+#define L1WN 4
+
+#define L2IW 5
+#define L2WN 8
+
+int main() {
+  auto l1d = cache_gen_l1<L1IW, L1WN, Data64B, MetadataBroadcastBase, ReplaceLRU, MSIPolicy, false, false, void, true>(NCore, "l1d");
+  auto core_data = get_l1_core_interface(l1d);
+  auto l1i = cache_gen_l1<L1IW, L1WN, Data64B, MetadataBroadcastBase, ReplaceLRU, MSIPolicy, false, true, void, true>(NCore, "l1i");
+  auto core_inst = get_l1_core_interface(l1i);
+  auto l2 = cache_gen_llc_randomized<L2IW, L2WN, Data64B, ReplaceSRRIP, void, true>(1, "l2")[0];
+  auto mem = new SimpleMemoryModel<Data64B,void,true>("mem");
+  SimpleTracer tracer(true);
+
+  for(int i=0; i<NCore; i++) {
+    l1i[i]->outer->connect(l2->inner, l2->inner->connect(l1i[i]->outer, true));
+    l1d[i]->outer->connect(l2->inner, l2->inner->connect(l1d[i]->outer));
+    l1i[i]->attach_monitor(&tracer);
+    l1d[i]->attach_monitor(&tracer);
+  }
+  l2->outer->connect(mem, mem->connect(l2->outer));
+
+  l2->attach_monitor(&tracer);
+  mem->attach_monitor(&tracer);
+
+  RegressionGen<NCore, true, false, PAddrN, SAddrN, Data64B> tgen;
+  return tgen.run(TestN, core_inst, core_data);
+}
