33f44ffef2920d76d148aca9a21c42254517d95a
wangzd12138 <wangzd12138@outlook.com>
2024-07-29 11:37:04 +0800

    add ccopy function to relocated-meta and use relocated-meta in remapping

--- cache/metadata.hpp
+++ cache/metadata.hpp
@@ -242,6 +242,12 @@ public:
   __always_inline void to_relocated()   { relocated = true;  }
   __always_inline void to_unrelocated() { relocated = false; }
   __always_inline bool is_relocated()   { return relocated;  }
+
+  __always_inline virtual void copy(const CMMetadataBase *m_meta) {
+    MT::copy(m_meta);
+    auto meta = static_cast<const MetadataWithRelocate<MT> *>(m_meta);
+    relocated = meta->relocated;
+  }
 };
 
 // A wrapper for implementing the multithread required cache line lock utility
