da4a7b02f97c0d98876711b85ba743d140119000
wangzd12138 <wangzd12138@outlook.com>
2024-09-19 16:00:22 +0800

    add swap function to support chameleon and dynamic random cache

--- cache/dynamic_random.hpp
+++ cache/dynamic_random.hpp
@@ -137,24 +137,14 @@ protected:
     cache->replace(c_addr, &new_ai, &new_idx, &new_way, XactPrio::acquire, replace_for_relocate);
     auto[m_meta, m_data] = cache->access_line(new_ai, new_idx, new_way);
     uint64_t m_addr = m_meta->addr(new_idx); 
-    auto c_m_meta = cache->meta_copy_buffer();
-    auto c_m_data = m_data ? cache->data_copy_buffer() : nullptr;
-
     if (m_meta->is_valid()) {
       if (static_cast<MT *>(m_meta)->is_relocated()) this->evict(m_meta, m_data, new_ai, new_idx, new_way, nullptr);
       else cache->hook_manage(m_addr, new_ai, new_idx, new_way, true, true, false, m_meta, m_data, nullptr);
     }
-    static_cast<CT *>(cache)->relocate(m_addr, m_meta, c_m_meta, m_data, c_m_data);
-
-    static_cast<CT *>(cache)->relocate(c_addr, c_meta, m_meta, c_data, m_data);
+    static_cast<CT *>(cache)->swap(m_addr, c_addr, m_meta, c_meta, m_data, c_data);
     cache->hook_read(c_addr, new_ai, new_idx, new_way, true, m_meta, m_data, nullptr);
-
     static_cast<MT *>(m_meta)->to_relocated();
     c_addr = m_addr;
-    static_cast<CT *>(cache)->relocate(m_addr, c_m_meta, c_meta, c_m_data, c_data);
-
-    cache->meta_return_buffer(c_m_meta);
-    cache->data_return_buffer(c_m_data);
   }
 
   void relocation_chain(uint32_t ai, uint32_t idx, uint32_t way) {
