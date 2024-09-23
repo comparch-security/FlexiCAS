ab3502017118075a02086abb865f737c23d07430
hanjinchi <1104722561@qq.com>
2024-09-23 16:11:51 +0800

    Remove evict from XactPrio

--- cache/coherence.hpp
+++ cache/coherence.hpp
@@ -14,7 +14,6 @@ struct XactPrio{
   static const uint16_t acquire       = 0x0001;
   static const uint16_t flush         = 0x0001;
   static const uint16_t probe         = 0x0010; // acquire miss, requiring lower cahce which back-probe this cache
-  static const uint16_t evict         = 0x0100; 
   static const uint16_t sync          = 0x0100; 
   static const uint16_t release       = 0x1000; // acquire hit but need back probe and writeback from inner
 };
@@ -255,17 +254,17 @@ protected:
     // evict a block due to conflict
     auto addr = meta->addr(s);
     assert(cache->hit(addr));
-    if constexpr (EnMT && Policy::evict_need_lock()) cache->set_mt_state(ai, s, XactPrio::evict);
     auto sync = Policy::writeback_need_sync(meta);
     if(sync.first) {
+      if constexpr (EnMT && Policy::sync_need_lock()) cache->set_mt_state(ai, s, XactPrio::sync);
       auto [phit, pwb] = probe_req(addr, meta, data, sync.second, delay); // sync if necessary
       if(pwb) cache->hook_write(addr, ai, s, w, true, false, meta, data, delay); // a write occurred during the probe
+      if constexpr (EnMT && Policy::sync_need_lock()) cache->reset_mt_state(ai, s, XactPrio::sync);
     }
     auto writeback = Policy::writeback_need_writeback(meta);
     if(writeback.first) outer->writeback_req(addr, meta, data, writeback.second, delay); // writeback if dirty
     Policy::meta_after_evict(meta);
     cache->hook_manage(addr, ai, s, w, true, true, writeback.first, meta, data, delay);
-    if constexpr (EnMT && Policy::evict_need_lock()) cache->reset_mt_state(ai, s, XactPrio::evict);
   }
 
   virtual std::tuple<bool, CMMetadataBase *, CMDataBase *, uint32_t, uint32_t, uint32_t>
@@ -349,10 +348,11 @@ protected:
       auto [probe, probe_cmd] = Policy::flush_need_sync(cmd, meta);
       if(!hit) return;
 
-      if constexpr (EnMT && Policy::evict_need_lock()) cache->set_mt_state(ai, s, XactPrio::evict);
       if(probe) {
+        if constexpr (EnMT && Policy::sync_need_lock()) cache->set_mt_state(ai, s, XactPrio::sync);
         auto [phit, pwb] = probe_req(addr, meta, data, probe_cmd, delay); // sync if necessary
         if(pwb) cache->hook_write(addr, ai, s, w, true, false, meta, data, delay); // a write occurred during the probe
+        if constexpr (EnMT && Policy::sync_need_lock()) cache->reset_mt_state(ai, s, XactPrio::sync);
       }
 
       auto writeback = Policy::writeback_need_writeback(meta);
@@ -361,10 +361,7 @@ protected:
       Policy::meta_after_flush(cmd, meta, cache);
       cache->hook_manage(addr, ai, s, w, hit, coh::is_evict(cmd), writeback.first, meta, data, delay);
 
-      if constexpr (EnMT) { 
-        if constexpr (Policy::evict_need_lock()) cache->reset_mt_state(ai, s, XactPrio::evict);
-        meta->unlock(); cache->reset_mt_state(ai, s, XactPrio::flush); 
-      }
+      if constexpr (EnMT) { meta->unlock(); cache->reset_mt_state(ai, s, XactPrio::flush); }
     }
   }
 
